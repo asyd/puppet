c9fff83257d69caabf4f186983f5873fd5974579
root <root@chucky.nnx.com>
2011-09-15 13:15:37 +0000

    New NRPE module

--- modules/mysql/manifests/server.pp
+++ modules/mysql/manifests/server.pp
@@ -1,9 +1,7 @@
 class mysql::server {
 	include common::debian
+	include nrpe::base
 
-	$packagesList = [ 'nagios-plugins-standard', 'nagios-nrpe-server', 'debconf-utils' ]
-
-	$nrpe_allowed_hosts = extlookup('nrpe_allowed_hosts')
 	$mysql_root_password = extlookup('mysql_root_password')
 	
 	common::debian::preseed_package { "mysql-server-5.1":
@@ -11,30 +9,12 @@ class mysql::server {
 		source => 'mysql/mysql-preseed.erb'
 	}
 
-	package { $packagesList:
-		ensure => installed
+	file { '/etc/nagios/.my.cnf':
+		require => [ Package['nagios-nrpe-server'], Package['mysql-server'] ],
+		content => template('mysql/nrpe-mysql.erb'),
+		notify => Service['nagios-nrpe-server'],
+		owner => nagios,
+		group => nagios,	
+		mode => 400
 	}
-
-#	service { 'nagios-nrpe-server':
-#		provider => debian,
-#		ensure => running
-#	}
-#
-#	file { '/etc/nagios/.my.cnf':
-#		require => [ Package['nagios-nrpe-server'], Package['mysql-server'] ],
-#		content => template('mysql/nrpe-mysql.erb'),
-#		notify => Service['nagios-nrpe-server'],
-#		owner => nagios,
-#		group => nagios,	
-#		mode => 400
-#	}
-#	
-#	file { '/etc/nagios/nrpe_local.cfg':
-#		notify => Service['nagios-nrpe-server'],
-#		require => Package['nagios-nrpe-server'],
-#		content => template('mysql/nrpe-local.cfg.erb'),
-#		owner => nagios,
-#		group => nagios,
-#		mode => 644
-#	}
 }
